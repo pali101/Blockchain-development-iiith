eada86dd4dcf6d78f01aec582eb2386be6629682
Aashish Paliwal <aashish.paliwal@live.com>
2024-09-18 21:41:49 +0530

    modified placeBet function, added custom error, ticketPrice

--- lottery.sol
+++ lottery.sol
@@ -5,14 +5,14 @@ contract Lottery {
     address public owner;
     address[] public players;
     uint16 public maxInput;
-    uint256 public maxPlayers;
+    uint64 public maxPlayers;
+    uint64 ticketPrice = 0.01 ether;
+
+    // Custom error to notify the user about the incorrect ticket price and sending correct ticket price back
+    error IncorrectTicketPrice(uint256 sentAmount, uint256 requiredAmount);
 
     // Event to log the bet placed by a player
-    event BetPlaced(
-        address indexed player,
-        uint256 amount,
-        uint16 predictedNumber
-    );
+    event BetPlaced(address indexed player, uint16 predictedNumber);
 
     // Event to log the winner of the lottery
     event WinnerDecided(
@@ -27,8 +27,35 @@ contract Lottery {
         _;
     }
 
-    constructor(uint16 _maxInput, uint256 _maxPlayers) payable {
+    constructor(
+        uint16 _maxInput,
+        uint64 _maxPlayers,
+        uint64 _ticketPrice
+    ) payable {
+        owner = msg.sender;
         maxInput = _maxInput;
         maxPlayers = _maxPlayers;
+        ticketPrice = _ticketPrice;
+    }
+
+    // Function to place a bet
+    function placeBet(uint16 _predictedNumber) public payable {
+        // require(msg.value == ticketPrice, "Incorrect ticket price");
+
+        if (msg.value != ticketPrice) {
+            revert IncorrectTicketPrice(msg.value, ticketPrice);
+        }
+
+        require(
+            players.length < maxPlayers,
+            "Max number of players reached. Try again later"
+        );
+        require(
+            _predictedNumber >= 0 && _predictedNumber <= maxInput,
+            "Predicted number should be between 0 and maxInput"
+        );
+
+        players.push(msg.sender);
+        emit BetPlaced(msg.sender, _predictedNumber);
     }
 }
